64744c7b353649bfc0f99408c108276c6fec1fe9
Antonios <AntoniosX@gmail.com>
2013-09-24 17:23:58 -0400

    initial commit
    test stop service functionality

---
+++ ProcessManager/ProcessManager/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceProcess;
+using System.Diagnostics;
+
+
+namespace ProcessManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            stopService("TTSimManager");
+            Console.WriteLine("Stopped TTSIM MGR");
+            Console.ReadKey();
+            stopService("guardian");
+            Console.WriteLine("Stopped guardian");
+            Console.ReadKey();
+            stopService("ttmd");
+            Console.WriteLine("Stopped ttmd");
+            Console.ReadKey();
+        }
+
+        private static void stopService(string service)
+        {
+            ServiceController srvc = new ServiceController(service);
+            srvc.Stop();
+            srvc.WaitForStatus(ServiceControllerStatus.Stopped, TimeSpan.FromSeconds(10));
+            if (!Equals(srvc.Status, ServiceControllerStatus.Stopped))
+            {
+                Process[] proc = Process.GetProcessesByName(service);
+                proc[0].Kill();
+            }
+        }
+    }
+}

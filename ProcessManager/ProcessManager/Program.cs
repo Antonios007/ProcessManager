8ec835a3dfa0ce77735e0b4feb6993a674926004
Antonios <AntoniosX@gmail.com>
2013-09-26 13:38:21 -0400

    modified code + cleaned up repository
    I removed files that should have been ignored, and setup clickonce on a
    failed attempt.  code was modified to display an error message at the
    end if any exceptions occurred

--- ProcessManager/ProcessManager/Program.cs
+++ ProcessManager/ProcessManager/Program.cs
@@ -2,30 +2,23 @@
 using System.Threading;
 using System.ServiceProcess;
 using System.Diagnostics;
-
+using System.Collections.Generic;
 
 namespace ProcessManager
 {
     class Program
     {
+        static List<string> finalMessage = new List<string>();
+        static bool display_final_message = false;
+
         static void Main(string[] args)
         {
-            Console.Write("Restarting TT");
+            Console.Write("Restarting TT Services");
             TimerCallback callback = new TimerCallback(Tick);
             // create a one second timer tick
             Timer stateTimer = new Timer(callback, null, 0, 1000);
 
-            Process[] proc = Process.GetProcessesByName("x_trader");
-            if (proc.Length > 0)
-            {
-                //works but X-trader popup confirm stops exit
-                //proc[0].CloseMainWindow();
-                try
-                {
-                    proc[0].Kill();
-                }
-                catch { Console.WriteLine("\nX_TRADER MUST BE RE-STARTED!"); }
-            }
+            stopProcess("x_trader");
 
             stopService("TTSimManager");
             stopService("guardian");
@@ -38,6 +31,37 @@ namespace ProcessManager
 
             Process.Start("C:\\tt\\Guardian\\GuardianStart.exe");
             Console.WriteLine("Guardian Re-started");
+
+            if (display_final_message)
+            {
+                Console.WriteLine("ERROR MESSAGES:");
+                foreach (string line in finalMessage)
+                {
+                    Console.WriteLine(line);
+                }
+                Console.WriteLine("Press any key to close this box");
+                Console.ReadKey();
+            }
+        }
+
+        private static void stopProcess(string process)
+        {
+            Process[] proc = Process.GetProcessesByName(process);
+            if (proc.Length > 0)
+            {
+                //works but X-trader popup confirm stops exit
+                //proc[0].CloseMainWindow();
+                try
+                {
+                    proc[0].Kill();
+                }
+                catch (Exception ex)
+                {
+                    display_final_message = true;
+                    finalMessage.Add(ex.Message);
+                    finalMessage.Add("X_TRADER must be stopped and re-started.");
+                }
+            }
         }
 
         private static void Tick(Object stateInfo)
@@ -72,7 +96,8 @@ namespace ProcessManager
                     { Console.WriteLine("\nProcess Killed : {0}", proc[0].ProcessName); }
                     else
                     {
-                        Console.WriteLine("ERROR: {0}\nHIT ANY KEY TO CONTINUE\nYou may need to run this application again.", ex.Message);
+                        display_final_message = true;
+                        finalMessage.Add(string.Format("ERROR: {0}\nYou may need to run this application again.", ex.Message));
                     }
                 }
             }
@@ -89,7 +114,10 @@ namespace ProcessManager
 
             }
             catch (Exception ex)
-            { Console.WriteLine(ex.ToString()); }
+            {
+                display_final_message = true;
+                finalMessage.Add(ex.ToString()); 
+            }
         }
     }
 }
